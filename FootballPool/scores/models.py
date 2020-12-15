ba82870638ed949f95ec8d7cfba03a9b58b05a7e
zanechoudhry <zanechoudhry@gmail.com>
2020-12-14 22:13:43 -0800

    League ID

--- FootballPool/scores/models.py
+++ FootballPool/scores/models.py
@@ -6,6 +6,5 @@ from django.db import models
 class team(models.Model):
     team_name = models.CharField(max_length=255)
 
-
 class winners(models.Model):
     winning_teams = models.ManyToManyField(team)
